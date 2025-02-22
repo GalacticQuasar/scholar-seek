70e94910bf7b31ce15699b8f1a9f7225c7616aad
ObviAvi <aggarwal.avi@gmail.com>
2025-02-22 18:55:07 -0500

    Added keyword functionality

--- src/pages/api/sendoff.js
+++ src/pages/api/sendoff.js
@@ -2,9 +2,11 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default async function handler(req, res) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-    const prompt = req.body.inputText;
+    const prompt = 
+        req.body.inputText + 
+        "\n Given this research paper draft, give me a list of around 5 specific, space seperated keywords that would best describe the paper";
 
     const result = await model.generateContent(prompt);
     console.log(result.response.text());
