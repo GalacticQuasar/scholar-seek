484252becd4eed3fe31f893fa3e8fc49c986d186
GalacticQuasar <akashdfw@gmail.com>
2026-01-11 19:10:53 -0500

    fix: upgrade gemini for free tier

--- src/pages/api/searchpapers.js
+++ src/pages/api/searchpapers.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     let url = req.body.downloadUrl;
 
--- src/pages/api/sendoff.js
+++ src/pages/api/sendoff.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       };
 
     const model = genAI.getGenerativeModel({
-        model: "gemini-2.0-flash",
+        model: "gemini-2.5-flash",
         generationConfig: {
           responseMimeType: "application/json",
           responseSchema: schema,
